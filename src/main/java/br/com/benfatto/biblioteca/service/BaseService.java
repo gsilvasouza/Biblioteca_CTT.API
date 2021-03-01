ef8fa1d740117a2cc795107b7107f59d68542109
Gabriel-Sila-Souza <gsilvasouza2000@gmail.com>
2021-03-01 18:49:01 -0300

    Criação da base service.

--- src/main/java/br/com/benfatto/biblioteca/service/BaseService.java
+++ src/main/java/br/com/benfatto/biblioteca/service/BaseService.java
@@ -1,4 +1,21 @@
 package br.com.benfatto.biblioteca.service;
 
-public interface BaseService {
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public interface BaseService<T, ID> {
+    List<T> findAll();
+
+    List<T> findAll(Sort sort);
+
+    T findById(ID id);
+
+    T create(T t);
+
+    T update(T t);
+
+    void delete(T t);
+
+    void deleteById(ID id);
 }
