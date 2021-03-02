44b21ca50eb22dea73899aad18afe5684f402dd5
Gabriel-Sila-Souza <gsilvasouza2000@gmail.com>
2021-03-02 00:10:16 -0300

    Criação dos DTO livro e categoria

--- src/main/java/br/com/benfatto/biblioteca/dto/CategoriaDTO.java
+++ src/main/java/br/com/benfatto/biblioteca/dto/CategoriaDTO.java
@@ -1,2 +1,14 @@
-package br.com.benfatto.biblioteca.dto;public class CategoriaDTO {
+package br.com.benfatto.biblioteca.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter(AccessLevel.PUBLIC)
+@Setter(AccessLevel.PUBLIC)
+public class CategoriaDTO {
+    private Integer id;
+    private String categoria;
 }
--- src/main/java/br/com/benfatto/biblioteca/dto/LivroDTO.java
+++ src/main/java/br/com/benfatto/biblioteca/dto/LivroDTO.java
@@ -1,2 +1,21 @@
-package br.com.benfatto.biblioteca.dto;public class LivroDTO {
+package br.com.benfatto.biblioteca.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor
+@Getter(AccessLevel.PUBLIC)
+@Setter(AccessLevel.PUBLIC)
+public class LivroDTO {
+        @JsonProperty("codigo")
+        private Integer id;
+        private String titulo;
+        private String autor;
+        private String categoria;
+        @JsonProperty("disponibilidade")
+        private Boolean status;
 }
