324933a4b32280271e8eeae29c9f40f0cc7b466f
Gabriel-Sila-Souza <gsilvasouza2000@gmail.com>
2021-03-02 14:23:54 -0300

    Atualização do model Livro.

--- src/main/java/br/com/benfatto/biblioteca/model/Livro.java
+++ src/main/java/br/com/benfatto/biblioteca/model/Livro.java
@@ -1,7 +1,15 @@
 package br.com.benfatto.biblioteca.model;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@NoArgsConstructor
+@Getter(AccessLevel.PUBLIC)
+@Setter(AccessLevel.PUBLIC)
 @Entity
 @Table(name="TB_LIVRO")
 public class Livro {
