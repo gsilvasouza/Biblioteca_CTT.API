51042c3b7277d4bfac898dc99da8b418c482ba9a
Gabriel-Sila-Souza <gsilvasouza2000@gmail.com>
2021-03-07 14:44:50 -0300

    Inserção de dos metodos de busca por: autor, titulo e categoria.

--- src/main/java/br/com/benfatto/biblioteca/repository/LivroRepository.java
+++ src/main/java/br/com/benfatto/biblioteca/repository/LivroRepository.java
@@ -1,9 +1,17 @@
 package br.com.benfatto.biblioteca.repository;
 
+import br.com.benfatto.biblioteca.model.Categoria;
 import br.com.benfatto.biblioteca.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Integer> {
+    Optional<List<Livro>> findAllByTitulo(String titulo);
+    Optional<List<Livro>> findAllByAutor(String autor);
+    Optional<List<Livro>> findAllByCategoria(Categoria categoria);
+    Optional<Livro> findByTitulo(String titulo);
 }
