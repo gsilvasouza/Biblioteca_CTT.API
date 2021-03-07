04b5bb07c27bae60d54e81fc9d1edd41495b3256
Gabriel-Sila-Souza <gsilvasouza2000@gmail.com>
2021-03-07 14:57:09 -0300

    Atualização da implemnetação da interface cliente.

--- src/main/java/br/com/benfatto/biblioteca/service/ClienteService.java
+++ src/main/java/br/com/benfatto/biblioteca/service/ClienteService.java
@@ -3,4 +3,5 @@ package br.com.benfatto.biblioteca.service;
 import br.com.benfatto.biblioteca.model.Cliente;
 
 public interface ClienteService extends BaseService<Cliente, Integer> {
+    Cliente findByCpf(String cpf);
 }
