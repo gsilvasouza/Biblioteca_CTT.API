3359a008a6dca707649ef4333289415f6fafde3a
Gabriel-Sila-Souza <gsilvasouza2000@gmail.com>
2021-03-07 14:44:38 -0300

    Inserção de dos metodos de busca por: autor, titulo e categoria.

--- src/main/java/br/com/benfatto/biblioteca/service/LivroService.java
+++ src/main/java/br/com/benfatto/biblioteca/service/LivroService.java
@@ -1,6 +1,13 @@
 package br.com.benfatto.biblioteca.service;
 
+import br.com.benfatto.biblioteca.model.Categoria;
 import br.com.benfatto.biblioteca.model.Livro;
 
+import java.util.List;
+
 public interface LivroService extends BaseService<Livro, Integer> {
+    List<Livro> findAllByTitulo(String titulo);
+    List<Livro> findAllByAutor(String autor);
+    List<Livro> findAllByCategoria(Categoria categoria);
+    Livro findByTitulo(String titulo);
 }
--- src/main/java/br/com/benfatto/biblioteca/service/LivroServiceImpl.java
+++ src/main/java/br/com/benfatto/biblioteca/service/LivroServiceImpl.java
@@ -1,13 +1,49 @@
 package br.com.benfatto.biblioteca.service;
 
+import br.com.benfatto.biblioteca.model.Categoria;
 import br.com.benfatto.biblioteca.model.Livro;
+import br.com.benfatto.biblioteca.repository.LivroRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
 @Service
 public class LivroServiceImpl extends BaseServiceImpl<Livro, Integer> implements LivroService {
+    private final LivroRepository livroRepository;
 
-    public LivroServiceImpl(JpaRepository<Livro, Integer> repository) {
+    public LivroServiceImpl(JpaRepository<Livro, Integer> repository, LivroRepository livroRepository) {
         super(repository);
+        this.livroRepository = livroRepository;
+    }
+
+
+    @Override
+    public List<Livro> findAllByTitulo(String titulo) {
+        return this.livroRepository.findAllByTitulo(titulo)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Nenhum livro encontrado com o titulo %s", titulo)));
+    }
+
+    @Override
+    public List<Livro> findAllByAutor(String autor) {
+        return this.livroRepository.findAllByAutor(autor)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Nenhum livro encontrado com o autor %s", autor)));
+    }
+
+    @Override
+    public List<Livro> findAllByCategoria(Categoria categoria) {
+        return this.livroRepository.findAllByCategoria(categoria)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Nenhum livro encontrado com a categoria %s", categoria.getCategoria())));
+    }
+
+    @Override
+    public Livro findByTitulo(String titulo) {
+        return this.livroRepository.findByTitulo(titulo)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Nenhum livro encontrado com o titulo %s", titulo)));
     }
 }
