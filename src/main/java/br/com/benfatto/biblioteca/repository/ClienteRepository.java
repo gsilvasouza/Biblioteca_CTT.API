d01b306120760d3d7910c6dc8825668032924750
Gabriel-Sila-Souza <gsilvasouza2000@gmail.com>
2021-03-07 14:57:28 -0300

    Atualização da repository cliente.

--- src/main/java/br/com/benfatto/biblioteca/repository/ClienteRepository.java
+++ src/main/java/br/com/benfatto/biblioteca/repository/ClienteRepository.java
@@ -4,6 +4,9 @@ import br.com.benfatto.biblioteca.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    Optional<Cliente> findByCPF(String cpf);
 }
